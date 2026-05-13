0314bbd1983f48434f7545eded3877fbd727040f
Janari Altdorf <jaltdorf@tlu.ee>
2026-05-13 20:28:19 +0300

    Initial commit

---
+++ kultuuriranits_backend/src/main/java/ee/meeskond7/Kultuuriranits/KultuuriranitsApplication.java
@@ -0,0 +1,13 @@
+package ee.meeskond7.Kultuuriranits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KultuuriranitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KultuuriranitsApplication.class, args);
+	}
+
+}
---
+++ kultuuriranits_backend/src/test/java/ee/meeskond7/Kultuuriranits/KultuuriranitsApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.meeskond7.Kultuuriranits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KultuuriranitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
