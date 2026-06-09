cf821b404e9152accf41cb3257cfe6297c1caebe
lisettereins <lreins@tlu.ee>
2026-06-09 11:13:04 +0300

    feat: add single program view

---
+++ kultuuriranits_frontend/app/programs/[id]/page.tsx
@@ -0,0 +1,78 @@
+import getProgram from "@/app/lib/program";
+
+const API_URL = process.env.NEXT_PUBLIC_BACK_URL;
+export default async function ProgramPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = await params;
+  const program = await getProgram(id);
+
+  console.log(console.log("params:", id));
+
+  if (!program) {
+    return <div>Programmi ei leitud.</div>;
+  }
+  
+
+  const details = [
+    ["Hind", `${program.pricePerStudent}€`],
+    ["Kestus", `${program.durationMinutes} min`],
+    ["Asukoht", program.location],
+    ["Keel", program.language],
+    ["Sihtgrupp", program.targetGroup],
+    ["Grupi suurus", `${program.minGroupSize} - ${program.maxGroupSize}`],
+    ["Staatus", program.status],
+  ];
+
+  return (
+    <div>
+      <div
+        key={program.id}
+        style={{
+          border: "1px solid gray",
+          padding: "16px",
+          borderRadius: "8px",
+        }}
+      >
+        <h2>{program.title}</h2>
+        <img
+          src={`${API_URL}/program/${program.id}/image`}
+          alt={program.title}
+          style={{
+            width: "100%",
+            height: "250px",
+            objectFit: "cover",
+            borderRadius: "8px",
+            marginBottom: "12px",
+          }}
+        />
+
+        {program.category && (
+          <span
+            style={{
+              display: "inline-block",
+              backgroundColor: "#e0e0e0",
+              padding: "4px 8px",
+              borderRadius: "4px",
+              fontSize: "12px",
+              marginBottom: "10px",
+              fontWeight: "bold",
+            }}
+          >
+            {program.category.name ?? `Kategooria ${program.category.id}`}
+          </span>
+        )}
+
+        <p>{program.description}</p>
+
+        {details.map(([label, value]) => (
+          <p key={label}>
+            <strong>{label}:</strong> {value}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+}
