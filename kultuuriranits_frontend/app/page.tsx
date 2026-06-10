a35be3f693d3ae646d55116b7f0726813f6644fd
Raimond Lige <raimlige@tlu.ee>
2026-06-10 13:57:56 +0300

    FE: quick homepage fix

--- kultuuriranits_frontend/app/page.tsx
+++ kultuuriranits_frontend/app/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { HeroCarousel } from '../components/home/HeroCarousel';
 import { CategoryCarousel } from '../components/home/CategoryCarousel';
 import { HowItWorks } from '../components/home/HowItWorks';
@@ -7,24 +6,22 @@ import { UserRoles } from '../components/home/UserRoles';
 
 export default function Home() {
   return (
-    <main>
-      <div className="flex flex-col" />
+    <main className="flex flex-col">
       {/* Hero Section - Full Width */}
       <HeroCarousel />
 
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 w-full">
-          {/* Categories Section */}
-          <CategoryCarousel />
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 w-full">
+        {/* Categories Section */}
+        <CategoryCarousel />
 
-          {/* How It Works Section */}
-          <HowItWorks />
+        {/* How It Works Section */}
+        <HowItWorks />
 
-          {/* For Whom Section */}
-          <UserRoles />
+        {/* For Whom Section */}
+        <UserRoles />
 
-          {/* Popular Programs Section */}
-          <PopularPrograms />
-        </div>
+        {/* Popular Programs Section */}
+        <PopularPrograms />
       </div>
     </main>
   );
